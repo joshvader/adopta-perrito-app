9b03c8d606ca0efdbe905fdc31950b03b1acac35
proyect_icaro <josuevalenzuela87@gmail.com>
2024-04-24 16:46:28 -0300

    files update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://joshvader.github.io/adopta-perrito-app"
+  base: "/adopta-perrito-app/",
 })
